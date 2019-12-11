802899be5a8fb41af9cae182deabc5849e2dbec1
AravindhSiva <adamaravindh@gmail.com>
2019-12-11 13:31:24 +0530

    Updates

---
+++ precache-manifest.206b9733ed28e26e71ae0f6443e314e5.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68b1b96b2da859ef4b1a7f94244959e4",
+    "url": "/cocktail/index.html"
+  },
+  {
+    "revision": "ea6dcbf1ad538f731c6e",
+    "url": "/cocktail/static/css/2.b137606a.chunk.css"
+  },
+  {
+    "revision": "e80bfda5decd8c4f0662",
+    "url": "/cocktail/static/css/main.7b5d190d.chunk.css"
+  },
+  {
+    "revision": "ea6dcbf1ad538f731c6e",
+    "url": "/cocktail/static/js/2.6b80735c.chunk.js"
+  },
+  {
+    "revision": "8cc6190161419c8bbcad791520efee63",
+    "url": "/cocktail/static/js/2.6b80735c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e80bfda5decd8c4f0662",
+    "url": "/cocktail/static/js/main.dcf8a586.chunk.js"
+  },
+  {
+    "revision": "692ed4b5125454fd8176",
+    "url": "/cocktail/static/js/runtime-main.d5b97f54.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/cocktail/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/cocktail/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/cocktail/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/cocktail/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/cocktail/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "85f18fbc3f9ed4792ef8b85c979e43ab",
+    "url": "/cocktail/static/media/loader.85f18fbc.gif"
+  }
+]);
